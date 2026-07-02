9f66c39d6f8468bccf334c282217a0b8423471b4
protectorebay <protector.ebay@gmail.com>
2026-07-02 10:40:25 +0200

    Create sessions.js

---
+++ sessions.js
@@ -0,0 +1,27 @@
+const sessions = [
+
+    {
+        id:"newyork",
+        name:"NEW YORK SESSION",
+        timezone:"America/New_York"
+    },
+
+    {
+        id:"london",
+        name:"LONDON SESSION",
+        timezone:"Europe/London"
+    },
+
+    {
+        id:"tokyo",
+        name:"TOKYO SESSION",
+        timezone:"Asia/Tokyo"
+    },
+
+    {
+        id:"sydney",
+        name:"SYDNEY SESSION",
+        timezone:"Australia/Sydney"
+    }
+
+];
